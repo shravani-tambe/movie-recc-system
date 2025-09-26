4da4e8d85bb568da44e2c3448e15a9dc23a8ffe1
Shravani Tambe <154653607+shravani-tambe@users.noreply.github.com>
2025-09-26 11:45:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 
 def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=b1ebec86ca7dd514f8d923cfdd6f496d&language=en-US"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=TMDB_API_KEY&language=en-US"
     data = requests.get(url).json()
     poster_path = data.get('poster_path', '')
     return f"https://image.tmdb.org/t/p/w500/{poster_path}" if poster_path else ""
